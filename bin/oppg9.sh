e38e7bed4724096ad6aea741b99f24687722a4c6
Kristian Rosenvold <kristian@rosenvold.com>
2010-03-07 20:06:03 +0100

    Fixed oppg9 with shell script

--- bin/oppg9.sh
+++ bin/oppg9.sh
@@ -1,6 +1,9 @@
-echo "Enviktigfil" >>EnviktigFil.txt
+echo "EnUviktigfil" >>EnUviktigFil.txt
 git add .
 git commit -q -am"Ekstern endring nr 1" 
-git push origin HEAD:enAnnenBranch
-
+echo "Enviktigfil, uten denne går alt rett vest" >>EnviktigFil.txt
+git add .
+git commit -q -am"Ekstern endring nr 2" 
+git push -f -q origin HEAD:enAnnenBranch
+git reset -q --hard HEAD@{2}
 echo "Oppgave 9 (Cherry Pick) er satt opp"
